74d8dbdb7ad58787a2d6a53dd88a74698d755381
Nuriden Muktar <ratkum.2001@gmail.com>
2024-02-25 20:22:49 +0600

    create authentication

--- zapis/celery_app.py
+++ zapis/celery_app.py
@@ -1,6 +1,5 @@
 import os
 import time
-
 from celery import Celery
 from django.conf import settings
 
@@ -8,11 +7,18 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zapis.settings')
 
 app = Celery('zapis')
 app.config_from_object('django.conf:settings')
+
+# Устанавливаем broker_url
 app.conf.broker_url = settings.CELERY_BROKER_URL
+
+# Автоматически обнаруживаем задачи
 app.autodiscover_tasks()
 
 
 @app.task()
 def debug_task():
     time.sleep(20)
-    print('Hello form debug_task')
+    print('Hello from debug_task')
+
+# docker-compose run --rm web-app sh -c "python manage.py shell"
+# from celery_app import debug_task
